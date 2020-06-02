67722cf4e485e3bc27e1b8d759b674dd6f91d393
Bruce <lsq***@gmail.com>
2020-06-03 09:39:15 +1000

    Add an umd patten example with depdency

--- with_dependency.js
+++ with_dependency.js
@@ -1,28 +1,26 @@
-```js
-// create a UMD pattern
+// create a UMD pattern, with a dependency  module  ("depModule")
 
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD. Register as an anonymous module.
-    define(["exports"], factory);
+    define(["exports", "depModule"], factory);
   } else if (
     typeof exports === "object" &&
     typeof exports.nodeName !== "string"
   ) {
     // CommonJS
-    factory(exports);
+    factory(exports, require("./moduleFileName").depModule);
   } else {
     // Browser globals with namespace
     root.globalNameSpace =
       typeof globalNameSpace === "undefined" ? globalNameSpace : {};
-    factory(root.globalNameSpace);
+    factory(root.globalNameSpace, root.globalNameSpace.depModule);
   }
 })(this, function (exports) {
   "use strict";
-  function SubModuleName() {}
+  function AnotherSubModuleName() {}
 
   // attach properties to the exports object to define
   // the exported module properties.
-  exports.SubModuleName = SubModuleName;
+  exports.AnotherSubModuleName = AnotherSubModuleName;
 });
-```
